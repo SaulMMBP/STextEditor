9e6d874ca5df78eba99e3d5c879aff2ce064a98f
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-06-10 17:38:42 -0500

    Creación de proyecto y ventana principal

---
+++ src/xyz/saulmmbp/JTextEditorMainApp.java
@@ -0,0 +1,15 @@
+package xyz.saulmmbp;
+
+import xyz.saulmmbp.view.MainWindow;
+
+public class JTextEditorMainApp {
+    
+    private static MainWindow mainWindow;
+    
+    public static void main(String[] args) {
+
+        mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+    }
+
+}
---
+++ src/xyz/saulmmbp/view/MainWindow.java
@@ -0,0 +1,64 @@
+package xyz.saulmmbp.view;
+
+import java.awt.HeadlessException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+    private static final long serialVersionUID = 7062252125155422559L;
+
+    /**
+     * Constructor de la ventana principal.
+     * @throws HeadlessException
+     */
+    public MainWindow() throws HeadlessException {
+        
+        /* Asigna el título a la ventana */
+        setTitle("MyTextEditor");
+        
+        /* Asigna el tamaño de la ventana */
+        setSize(1280, 720);
+        
+        /* Asigna su estado extendido, es decir, su estado de inicio
+         * en este caso maximiza su ancho y su alto 
+         */
+        setExtendedState(MAXIMIZED_BOTH);
+        
+        /* Inicializa los componentes */
+        initComponents();
+    }
+
+    /**
+     * Inicializa los componentes de la ventana principal.
+     */
+    public void initComponents() {
+        
+        /* Asigna el Layout de la ventana */
+        getContentPane().setLayout(null);
+        
+        /* Agrega un listener a la ventana */
+        addWindowListener(new WindowAdapter() {
+            
+            /**
+             * Sobreescribe el método windowClosing para ejecutar el método exitForm 
+             * cuando se cierre la ventana
+             */
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exitForm(e);
+            }
+            
+        });
+        
+    }
+
+    /*
+     * Termina la ejecución de la máquina virtual de java
+     */
+    protected void exitForm(WindowEvent e) {
+        System.exit(0);
+    }
+}
