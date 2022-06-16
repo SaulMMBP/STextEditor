73bdf19b79b16a5976ad0b35d3c26d2df921228b
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-06-16 01:13:05 -0500

    Probando funciones de JEditorPane

--- src/main/xyz/saulmmbp/view/viewport/Viewport.java
+++ src/main/xyz/saulmmbp/view/viewport/Viewport.java
@@ -4,10 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
+import java.io.IOException;
+import java.net.URL;
 
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
-import javax.swing.text.BadLocationException;
 
 public class Viewport extends JPanel {
 
@@ -23,8 +24,6 @@ public class Viewport extends JPanel {
     private String fontType;
     private int fontStyle;
     
-    private String testText;
-    
     public Viewport() {
         
         /* Inicializando Componentes */
@@ -36,16 +35,13 @@ public class Viewport extends JPanel {
         /* Inicializando */
         editor = new JEditorPane() {
 
-            @Override
-            public boolean getScrollableTracksViewportWidth() {
-                return true;
-            }
+            private static final long serialVersionUID = -2043112489770676986L;
 
             @Override
-            public boolean getScrollableTracksViewportHeight() {
+            public boolean getScrollableTracksViewportWidth() {
                 return false;
             }
-            
+
         };
         
         width = 794;
@@ -62,7 +58,6 @@ public class Viewport extends JPanel {
         editor.setMargin(new Insets(20, 20, 20, 20));
         editor.setForeground(Color.BLACK);
         editor.setFont(font);
-        
         
 
         initDocument();
@@ -91,16 +86,12 @@ public class Viewport extends JPanel {
     }
     
     private void initDocument() {
-        this.testText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper eget massa sed tristique. Aliquam pellentesque, nulla vitae interdum accumsan, urna lorem vestibulum sem, sit amet mattis orci lectus at nisi. Fusce quis dapibus dui. Donec eleifend sit amet tellus sed gravida. Suspendisse imperdiet eros sem, eget laoreet dui aliquet nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut ut felis nulla. Sed sed risus in lectus aliquet ultricies. Sed vitae nunc dictum, tristique nulla vitae, posuere mi. Duis cursus vitae turpis ut ornare. Sed cursus lobortis urna vitae tincidunt. Fusce ornare ipsum at nisi molestie vulputate. Donec et scelerisque mi. Sed mattis rutrum egestas. Phasellus vulputate nisl non felis ultrices, eget efficitur nunc dignissim.\n"
-                + "\nDonec ac dolor a augue accumsan euismod. Praesent luctus urna ac velit bibendum eleifend. Quisque molestie, risus et cursus vestibulum, justo dui vulputate odio, interdum commodo dui lacus non augue. Maecenas tincidunt orci vitae hendrerit ornare. Mauris suscipit dolor eget nulla accumsan aliquet. Etiam elementum pretium augue at hendrerit. Aenean turpis lectus, iaculis commodo mattis vitae, viverra quis nisl. Mauris ac ultrices sapien, non gravida ligula. Sed facilisis nisl et lacus imperdiet eleifend. Maecenas vel dui lacinia, convallis nulla id, rhoncus velit. Praesent nisi erat, fermentum at dapibus eget, iaculis sed leo. In a ligula mollis, tempus ante ut, scelerisque erat. Suspendisse dui mauris, elementum laoreet lectus vitae, vehicula sagittis justo. Donec a interdum tortor, vel efficitur diam.\n"
-                + "\nDonec laoreet dui risus, pellentesque egestas mi porta sed. Etiam dignissim scelerisque purus molestie luctus. Fusce sapien dui, placerat id metus nec, egestas dapibus lectus. Duis tortor leo, viverra nec sollicitudin id, consequat non quam. Donec eget lectus sit amet erat viverra iaculis in egestas ante. Suspendisse mollis laoreet risus quis mollis. Pellentesque molestie gravida ligula ultrices accumsan. Nullam eget nunc ex. Maecenas in urna at lorem scelerisque ornare. Nullam ullamcorper, massa sit amet vulputate tincidunt, velit nibh cursus quam, rutrum convallis purus arcu elementum dolor.\n"
-                + "\nDuis ut vestibulum lacus. Vivamus id viverra massa. Phasellus vitae justo sollicitudin, dignissim metus a, hendrerit neque. Donec mollis sed magna eget tincidunt. Vivamus elit lacus, fringilla sed justo non, feugiat malesuada est. Nam sed nibh mi. Nunc ut ornare tortor, sed consequat urna. Nullam ac nisl id velit ullamcorper fringilla. Nam vel eros non ligula aliquet varius placerat a est. Proin varius, elit ac venenatis pharetra, tortor metus malesuada leo, a efficitur nisl nisi vel leo.\n"
-                + "\nMauris porta quam id ligula malesuada, aliquam auctor odio ullamcorper. Quisque quis mi porta, ultricies eros ac, euismod est. Maecenas at dapibus urna, ac fermentum orci. Nam placerat vitae lectus vitae commodo. Maecenas rhoncus libero bibendum diam dignissim, nec blandit dui molestie. Duis cursus sed lectus id pellentesque. Curabitur in egestas metus. Vestibulum in fringilla tellus. Maecenas eget urna sapien. Aenean ac auctor turpis. Vivamus ac maximus lectus.\n";
-    
+        URL url = Viewport.class.getResource("/html/image.html");
         try {
-            editor.getDocument().insertString(editor.getDocument().getLength(), testText, null);
-        } catch (BadLocationException e) {
+            editor.setPage(url);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        
     }
 }
