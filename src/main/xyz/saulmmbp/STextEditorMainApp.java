4cabd881571b2253de824abf2d5badeba6eda40f
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-08-06 22:30:59 -0500

    Rediseño completo de aplicación

--- src/main/xyz/saulmmbp/STextEditorMainApp.java
+++ src/main/xyz/saulmmbp/STextEditorMainApp.java
@@ -8,17 +8,22 @@ import xyz.saulmmbp.view.MainWindow;
 
 public class STextEditorMainApp {
     
+    /* Declaración de ventana */
     public static MainWindow mainWindow;
     
     public static void main(String[] args) {
 
+        /* Diseño de gui */
         try {
             UIManager.setLookAndFeel(new FlatDarkLaf());
         } catch (Exception e) {
             System.out.println("No se pudo establecer el aspecto deseado");
         }
         
+        /* Instancia de ventana principal */
         mainWindow = new MainWindow();
+        
+        /* Hace visible la ventana */
         mainWindow.setVisible(true);
     }
 
--- src/main/xyz/saulmmbp/view/MainWindow.java
+++ src/main/xyz/saulmmbp/view/MainWindow.java
@@ -1,36 +1,55 @@
 package xyz.saulmmbp.view;
 
-import java.awt.BorderLayout;
 import java.awt.HeadlessException;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
-
-import xyz.saulmmbp.view.menubar.SMenuBar;
-import xyz.saulmmbp.view.viewport.Viewport;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 
 public class MainWindow extends JFrame {
 
     private static final long serialVersionUID = -3354882865572732093L;
 
     /* Componentes */
-    private SMenuBar menuBar;
-    private SToolBar toolBar;
-    private StateBar stateBar;
+    private JMenuBar menuBar;
+    private JToolBar toolBar;
     private JScrollPane scrollPane;
-    private Viewport viewport;
+    private JTextArea textArea;
+    
+    private JMenu menuArchivo;
+    private JMenu menuEdicion;
+    private JMenu menuOpciones;
+    
+    private JMenu submnFuente;
+    private JMenu submnTamano;
+    
+    private JMenuItem mnItmSalir;
+    private JMenuItem mnItmCortar;
+    private JMenuItem mnItmCopiar;
+    private JMenuItem mnItmPegar;
+    private JMenuItem mnItmCourierNew;
+    private JMenuItem mnItmArial;
+    private JMenuItem mnItmFuentePred;
+    private JMenuItem mnItm16;
+    private JMenuItem mnItm24;
+    private JMenuItem mnItmTamanoPred;
+    
     
     public MainWindow() throws HeadlessException {
 
         /* Configura la ventana */
         setTitle("STextEditor");
-        setSize(1280, 720);
-
-        /*
-         * Asigna su estado extendido, es decir, su estado de inicio en este caso
-         * maximiza su ancho y su alto
-         */
-//        setExtendedState(MAXIMIZED_BOTH);
+        setSize(500, 300);
+        
+        /* Configuración para la acción de cerrar la ventana */
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         /* Inicializa los componentes */
         initComponents();
@@ -38,30 +57,116 @@ public class MainWindow extends JFrame {
 
     private void initComponents() {
         
-
-        /* Configuración para la acción de cerrar la ventana */
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        /* Inicializa componentes */
-        menuBar = new SMenuBar();
-        toolBar = new SToolBar();
-        stateBar = new StateBar();
+        /* Inicialización de componentes */
+        menuBar = new JMenuBar();
+        toolBar = new JToolBar();
         scrollPane = new JScrollPane();
-        viewport = new Viewport();
+        textArea = new JTextArea();
+        
+        menuArchivo = new JMenu();
+        menuEdicion = new JMenu();
+        menuOpciones = new JMenu();
+        
+        submnFuente = new JMenu();
+        submnTamano = new JMenu();
+        
+        mnItmSalir = new JMenuItem();
+        mnItmCortar = new JMenuItem();
+        mnItmCopiar = new JMenuItem();
+        mnItmPegar = new JMenuItem();
+        mnItmCourierNew = new JMenuItem();
+        mnItmArial = new JMenuItem();
+        mnItmFuentePred = new JMenuItem();
+        mnItm16 = new JMenuItem();
+        mnItm24 = new JMenuItem();
+        mnItmTamanoPred = new JMenuItem();
+        
+        /* Configuración de scrollPane */
+        scrollPane.setViewportView(textArea);
+        
+        /* Configuración de textArea */
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        
+        
+        /* Configuración de menus */
+        menuArchivo.setText("Archivo");
+        menuArchivo.setMnemonic('A');
+        
+        menuEdicion.setText("Edición");
+        menuEdicion.setMnemonic('E');
+        
+        menuOpciones.setText("Opciones");
+        menuOpciones.setMnemonic('O');
+        
+        submnFuente.setText("Fuente");
+        submnFuente.setMnemonic('F');
+        
+        submnTamano.setText("Tamaño");
+        submnTamano.setMnemonic('T');
+        
+        /* Configuración de Items de menú */
+        mnItmSalir.setText("Salir");
+        mnItmSalir.setMnemonic('S');
+        
+        mnItmCortar.setText("Cortar");
+        mnItmCortar.setMnemonic('R');
+        mnItmCortar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+        
+        mnItmCopiar.setText("Copiar");
+        mnItmCopiar.setMnemonic('C');
+        mnItmCopiar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
 
-        /* Configuraciones */
-            /* scrolls */
-        scrollPane.setViewportView(viewport);
+        mnItmPegar.setText("Pegar");
+        mnItmPegar.setMnemonic('P');
+        mnItmPegar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+        
+        mnItmCourierNew.setText("Courier New");
+        mnItmCourierNew.setMnemonic('C');
+        
+        mnItmArial.setText("Arial");
+        mnItmArial.setMnemonic('A');
+        
+        mnItmFuentePred.setText("Predeterminada");
+        mnItmFuentePred.setMnemonic('P');
+        
+        mnItm16.setText("16");
+        
+        mnItm24.setText("24");
+        
+        mnItmTamanoPred.setText("Predeterminado");
+        
+        /* añado items a menus y submenus */
+        menuArchivo.add(mnItmSalir);
+        
+        menuEdicion.add(mnItmCopiar);
+        menuEdicion.add(mnItmCortar);
+        menuEdicion.add(mnItmPegar);
+        
+        menuOpciones.add(submnFuente);
+        menuOpciones.add(submnTamano);
+        
+        submnFuente.add(mnItmCourierNew);
+        submnFuente.add(mnItmArial);
+        submnFuente.add(mnItmFuentePred);
+        
+        submnTamano.add(mnItm16);
+        submnTamano.add(mnItm24);
+        submnTamano.add(mnItmTamanoPred);
+        
+        /* añado menus a barra de menus */
+        menuBar.add(menuArchivo);
+        menuBar.add(menuEdicion);
+        menuBar.add(menuOpciones);
+        
+        /* añado componentes a la ventana */
+        add(toolBar);
+        add(scrollPane);
 
-        /* Agrega Componentes */
+        /* añado barra de menus a la ventana */
         setJMenuBar(menuBar);
-        add(toolBar, BorderLayout.NORTH);
-        add(stateBar, BorderLayout.SOUTH);
-        add(scrollPane, BorderLayout.CENTER);
+
     }
 
-    public void zoom(int porcentaje) {
-        viewport.zoom(porcentaje);
-    }
 
 }
